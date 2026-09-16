42eb9ce3ad0f3a91ff0c760c4d24c6e92c1dbefb
Gowthami <gowskrish89@gmail.com>
2026-09-16 19:32:53 +0530

    latest

---
+++ script/script.js
@@ -0,0 +1,16 @@
+console.log('abc');
+
+var a = 10;
+
+function x (){
+    console.log(a)
+    var b = 20;
+    function y(){
+        var c = 30;
+        console.log(b)
+        console.log(c)
+    }
+    return y
+}
+x()()
+console.log("end")
